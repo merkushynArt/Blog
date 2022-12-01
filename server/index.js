1fa3f3f89fa10a409b606e7d5f2cdbcab97784c7
Artem <merkushynwrite@gmail.com>
2022-12-01 16:00:27 +0200

    connected to mongo_db

--- server/index.js
+++ server/index.js
@@ -1,7 +1,23 @@
 import expess from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 const app = expess();
+dotenv.config();
 
-app.listen(5000, () => {
-   console.log('server started');
-})
+//Constants
+const PORT = process.env.PORT || 3001
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD
+const DB_NAME = process.env.DB_NAME
+
+async function start() {
+   try{
+      await mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.x1uwzow.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`);
+
+      app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
+   } catch(error) {
+      console,log(error)
+   }
+}
+start();
