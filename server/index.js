ede0865af138096b24db674b832c90d725b63bd9
Artem <merkushynwrite@gmail.com>
2022-12-02 20:14:02 +0200

    created created a user schema

--- server/index.js
+++ server/index.js
@@ -1,23 +1,34 @@
 import expess from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 const app = expess();
 dotenv.config();
 
 //Constants
-const PORT = process.env.PORT || 3001
-const DB_USER = process.env.DB_USER
-const DB_PASSWORD = process.env.DB_PASSWORD
-const DB_NAME = process.env.DB_NAME
+const PORT = process.env.PORT || 3001;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_NAME = process.env.DB_NAME;
+
+//Middleware
+//Ми можемо відправляти запроси до нашого бекенда з різних ір адрес
+app.use(cors());
+//Express буде розуміти що дані з фронту будуть приходити у форматі json
+app.use(expess.json());
+
+app.get('/', (req, res) => {
+   res.json({message: 'All is fine'});
+});
 
 async function start() {
    try{
-      await mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.x1uwzow.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`);
+      await mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.rss3cwg.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`);
 
-      app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
+      app.listen(PORT, () => console.log(`Server started on port: ${PORT}, http://localhost:3002/`));
    } catch(error) {
-      console,log(error)
+      console.log(error);
    }
 }
 start();
